845c1468fe31d28dca36f43cb41fb81ab0a9a2a1
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-01-23 22:22:19 +0200

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+// Change code below this line
+//1) Створення і рендер розмітки на підставі масиву даних 
+
+const galleryContainerItems = document.querySelector('.gallery');
+const createGalleryItems = galleryItemsCardsMarkup(galleryItems);
+galleryContainerItems.insertAdjacentHTML('afterbegin', createGalleryItems);
+
+function galleryItemsCardsMarkup(galleryItems) {
+return galleryItems
+	.map(({ description, original, preview}) => {
+		return `
+			<div class="gallery__item">
+			<a class="gallery__link" href="${original}" />
+			<img 
+				class="gallery__image" 
+				src="${preview}" 
+				data-source="${original}" 
+				alt="${description}" 
+			/>
+			</a>
+		</div>
+	`;
+})
+	.join('');
+}
